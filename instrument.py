63a74415d021a345ac8383971af67efba53ecc36
Tommaso Innocenti <innotommy@gmail.com>
2017-04-05 18:28:53 -0400

    Correct path of file and create the config for instrument.

--- instrument.py
+++ instrument.py
@@ -1,20 +1,25 @@
-import psycopg2,os,fnmatch,re
-import os.path,string
-import sys,json
-sys.path.append('/Users/innotommy91/qwer/src/third_party/WebKit/Source/bindings/scripts') #import folder for idl_reader
-from idl_reader import IdlReader
+import json
+
+
 
 def instrument():
 	instr = []
 	with open('targetfunction.txt') as f:
 		for line in f:
 			instr.append(json.loads(line))
-	file = open('instrument.json','a')
+	file = open('targetfunction.json','a')
 	temp =[]
 	for j in range(len(instr)):
 		tup={"filename":instr[j]['filecpp'],"function":instr[j]['method']}
 		temp.append(tup)
 	json.dump(temp,file)
+	file.close()
+	file = open('Deprecator_target.txt','a')
+	for j in range(len(instr)):
+		file.write(instr[j]['filecpp'])
+		file.write(":")
+		file.write(instr[j]['method'])
+		file.write("\n")
 	file.close()
 
 
